e12f57a15163adf1f47aab42114a712f5eb7a2c2
Matthew Thomas Dressa <mtd67@cornell.edu>
2021-01-18 15:32:49 -0500

    major changes

---
+++ docs/assets/js/mobile_version.js
@@ -0,0 +1,20 @@
+
+
+
+	function myFunction() {
+        var x = document.getElementById("mySidenav");
+        if (x.className === "sidenav") {
+          x.className += " responsive";
+        } else {
+          x.className = "sidenav";
+        }
+      }  
+
+    function openNav() {
+        document.getElementById("mySidenav").style.width = "250px";
+      }
+      
+      function closeNav() {
+        document.getElementById("mySidenav").style.width = "0";
+      }
+(jQuery);
